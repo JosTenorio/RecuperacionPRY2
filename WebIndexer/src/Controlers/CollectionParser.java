40321485e49df9bde9c67bc07ad3fb40b3f446f6
CarlosV <carosjvare@gmai.com>
2021-10-22 00:07:08 -0600

    Document finding and parsing finished

--- WebIndexer/src/Controlers/CollectionParser.java
+++ WebIndexer/src/Controlers/CollectionParser.java
@@ -3,11 +3,17 @@ package Controlers;
 import Models.Document;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
+import org.jsoup.Jsoup;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.regex.Pattern;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
 
 
 public class CollectionParser {
@@ -23,19 +29,53 @@ public class CollectionParser {
         try( LineIterator lineIterator = FileUtils.lineIterator(collection,"UTF-8"))
         {
             org.jsoup.nodes.Document doc = null;
-            String currentDocString = null;
+            String currentDocString = "";
             currentDocString += lineIterator.nextLine();
-            int docCount = 1;
+            int docCount = 0;
+            BigInteger byteCount = BigInteger.valueOf((Integer)(currentDocString.getBytes(StandardCharsets.UTF_8).length));
+            Pattern pathEndHtml =Pattern.compile("</html.*?>");
+            Pattern patHtml = Pattern.compile("<html.*?>");
+            Pattern patDoctype = Pattern.compile(".*?<!DOCTYPE.*?>");
             while(lineIterator.hasNext())
             {
+                BigInteger documentStart;
                 String currentLine = lineIterator.nextLine();
-                if(Objects.equals(currentLine, "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"))
+                BigInteger linebytesize =  BigInteger.valueOf((Integer)(currentLine.getBytes(StandardCharsets.UTF_8).length));
+                byteCount = byteCount.add(linebytesize);
+                if(patDoctype.matcher(currentLine).matches())
                 {
+                    // If we find a possible html start point we save that byte count to index later
+                    documentStart = byteCount;
+                }
+                else if(patHtml.matcher(currentLine).matches())
+                {
+
+                    // If we find an opening html tag then we need to parse all the content into a single string to open the document in jsoup
+                    String documentSource = "";
+                    documentSource+=currentLine;
+                    while(!pathEndHtml.matcher((currentLine)).matches())
+                    {
+                        // Gets next line and adds it to the source string
+                        currentLine = lineIterator.nextLine();
+                        documentSource+=currentLine;
+                        linebytesize =  BigInteger.valueOf((Integer)(currentLine.getBytes(StandardCharsets.UTF_8).length));
+                        byteCount = byteCount.add(linebytesize);
+                    }
+                    //End of the doc
+                    BigInteger documentEnd = byteCount;
+                    org.jsoup.nodes.Document document = Jsoup.parse(documentSource);
+
+                    Elements Tags = document.select("h1, h2, h3, h4, h5, h6");
+                    for(Element tag: Tags){
+                        System.out.println(tag.text());
+                    }
                     docCount++;
+
                 }
 
 
             }
+            System.out.println(byteCount);
             System.out.println("Habían "+docCount+" páginas");
         } catch (IOException e)
         {
