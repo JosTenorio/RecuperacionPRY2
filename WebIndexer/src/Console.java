a08a3a7772de1e24339d42a9270ea97e3144cebe
Joseph <jostenoriop@gmail.com>
2021-10-23 11:24:45 -0600

    Finished console structure

---
+++ WebIndexer/src/Console.java
@@ -0,0 +1,49 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Console {
+
+    final static Scanner ui = new Scanner(System.in);
+
+
+    public static void showMainMenu() {
+        int option;
+        String error = null;
+        while (true) {
+            System.out.println("\nBienvenido al sistema de indexado de páginas web");
+            System.out.println("1. Indexar colecciones");
+            System.out.println("2. Realizar consultas");
+            System.out.println("3. Salir del programa");
+            System.out.print("Favor digite el número de su selección: ");
+
+            try {
+                option = ui.nextInt();
+            } catch (InputMismatchException exception) {
+                option = -1;
+                error = ui.next();
+            }
+            if (error != null) {
+                System.out.println("Error, no se reconoce al comando: '" + error + "', favor reintentar");
+                error = null;
+                continue;
+            }
+            switch (option) {
+                case 1 -> indexCollection();
+                case 2 -> queryIndex();
+                case 3 -> {
+                    System.out.println("Terminando ejecución");
+                    System.exit(0);
+                }
+                default -> System.out.println("Error, no se reconoce al comando: '" + Integer.toString(option) + "', favor reintentar");
+            }
+        }
+    }
+
+    private static void indexCollection() {
+        System.out.println("Indizar colección");
+    }
+
+    private static void queryIndex() {
+        System.out.println("Indizar colección");
+    }
+}
--- WebIndexer/src/main.java
+++ WebIndexer/src/main.java
@@ -5,16 +5,6 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
-        String collectionPath = null;
-        System.out.printf("Por favor ingrese la ruta de la colleción: \n >>");
-        try{
-            Scanner scanner = new Scanner(System.in);
-            collectionPath = scanner.nextLine();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        CollectionParser col = new CollectionParser(collectionPath);
-        col.readFile();
-
+        Console.showMainMenu();
 }
 }
