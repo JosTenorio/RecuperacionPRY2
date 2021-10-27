6634ccbf355056c4be334b0d6c161e3d53000029
Joseph <jostenoriop@gmail.com>
2021-10-27 17:31:38 -0600

    Started tile removal filter fix

--- WebIndexer/src/main.java
+++ WebIndexer/src/main.java
@@ -1,9 +1,9 @@
-import Controlers.CollectionHandler;
-
 import java.io.IOException;
+import Controlers.CollectionHandler;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        Console.showMainMenu();
+        //Console.showMainMenu();
+        CollectionHandler.testAnalyzer("La tarea consiste en programar una niña aplicación que permita indexar y buscar una colección de páginas web. Se debe usar la biblioteca ", "Stopwords\\Stopwords.txt");
     }
 }
